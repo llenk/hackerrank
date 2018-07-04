8d72134586c3c2623c0158ef9c1f04689da765ca
llenk <ellenkeal@gmail.com>
2018-07-04 14:34:12 -0500

    birthday chocolate

--- birthdayChocolate.js
+++ birthdayChocolate.js
@@ -1,6 +1,15 @@
 function solve(s, d, m) {
-  
-
+  let numSegments = 0;
+  for (let i = 0; i < s.length - m + 1; i++) {
+    let sum = 0;
+    for (let j = 0; j < m; j++) {
+      sum += s[i + j];
+    }
+    if (sum == d) {
+      numSegments++;
+    }
+  }
+  return numSegments;
 }
 
 console.log(solve([1,2,1,3,2], 3,2));
